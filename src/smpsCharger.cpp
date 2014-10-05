75182f1973f3eecd54955f0aeed2691d63f72d8e
miceuz <mic@wemakethings.net>
2014-10-05 12:01:18 +0300

    put the main charging logic into order, add precharge stage, add some logic for temp compensation

--- src/smpsCharger.cpp
+++ src/smpsCharger.cpp
@@ -3,78 +3,135 @@
 #include "smpsCharger.h"
 #include "Psu.h"
 
+//milliseconds
 #define VOLTAGE_CTRL_INTERVAL 100
 
-#define TOPPING_VOLTAGE 14400
-#define FLOAT_VOLTAGE 13650
+uint16_t avgCurrent = 0;
+uint16_t avgVoltage = 0;
 
-int avgCurrent = 0;
-unsigned long absorptionStart = 0;
-unsigned long lastVoltageControlTimestamp = 0;
+unsigned long stageStarted = 0;
+unsigned long lastControlTimestamp = 0;
 
-Psu psu(DAC_OUT, ISENSE, VSENSE, {1,1,1}, {1,1,1});
+uint8_t mode;
 
-byte isVoltageControlTime() {
-  if(millis() - lastVoltageControlTimestamp >= VOLTAGE_CTRL_INTERVAL){
-    lastVoltageControlTimestamp = millis();
+PID voltagePid = {7.1, 1.50, 0};
+PID currentPid = {2.1, 0.05, 0}; 
+
+Psu psu(DAC_OUT, ISENSE, VSENSE, &voltagePid, &currentPid);
+
+//1.7V @ 25°C
+uint16_t criticallyDischargedVoltage = 11700;
+
+//for 100AH battery
+uint16_t bulkCurrent = 10000;
+uint16_t prechargeCurrent = bulkCurrent / 10;
+uint16_t bulkEndCurrent = bulkCurrent / 10;
+
+//2.4V per cell
+uint16_t absorptionVoltage = 14400;
+//2.275V per cell
+uint16_t floatVoltage = 13650;
+
+void setBulkCurrent(uint16_t c) {
+  bulkCurrent = c;
+  prechargeCurrent = bulkCurrent / 10;
+  bulkEndCurrent = bulkCurrent / 10;
+}
+
+byte isControlTime() {
+  if(millis() - lastControlTimestamp >= VOLTAGE_CTRL_INTERVAL){
+    lastControlTimestamp = millis();
     return 1;
   }
   return 0;
 }
 
-void setup()  { 
+int8_t getBatteryTemperature() {
+  return 25;
+}
+
+//battery temp coeff -3.9 mV/C°/cell = -0.0234 V/C°
+int16_t voltsPerDegreeCompensation() {
+  return (int16_t)(getBatteryTemperature() - 20) * -234 / 10;
+}
+
+void startPrechargeState() {
+  Serial.println("Starting PRECHARGE");
+  mode = MODE_PRECHARGE;
+  psu.setConstantCurrent(prechargeCurrent);
+  stageStarted = millis();
+}
+
+void startBulkStage() {
+  Serial.println("Starting BULK CHARGE");
+  mode = MODE_BULK; 
+  psu.setConstantCurrent(bulkCurrent);
+  stageStarted = millis();
+}
+
+void startAbsorptionStage() {
+  Serial.println("Starting ABSORPTION CHARGE");
+  mode = MODE_ABSORPTION; 
+  psu.setConstantVoltage(absorptionVoltage);
+  stageStarted = millis();
+}
+
+void startFloatStage() {
+  Serial.println("Starting FLOATING CHARGE");
+  mode = MODE_FLOAT; 
+  psu.setConstantVoltage(floatVoltage);
+  stageStarted = millis();
+}
+
+void checkBatteryConnected() {
+  if(millis() - stageStarted > 3000){
+    if(psu.getCurrent() < 10) {
+      Serial.println("Connect the battery!");
+      while(1);
+    }
+  }
+}
+
+void setup() {
   Serial.begin(9600);
+
   analogReference(INTERNAL);
   pinMode(DAC_OUT, OUTPUT);
   digitalWrite(DAC_OUT, 0);
 
-  unsigned int voltage = map(analogRead(VSENSE), 0, 1023, 0, MAX_VOLTAGE);//millivolts
-  Serial.print("Voltage ");
-  Serial.println(voltage);
-
-  while(voltage <= 100) {
-    Serial.println("Connect the battery");
-    delay(1000);
-    voltage = map(analogRead(VSENSE), 0, 1023, 0, MAX_VOLTAGE);//millivolts
-  }  
-  psu.setConstantCurrent(10000);
+  setBulkCurrent(10000);
+  startPrechargeState();
 } 
 
-
 void loop()  { 
-
-  avgCurrent = (float)psu.getCurrent() * 0.005 + (float)avgCurrent*0.995;
-  
-  psu.servo();
-  
-  if(MODE_CV == psu.getMode() && isVoltageControlTime()) {
-    Serial.print("CV, ");
-    if(psu.getVoltage() >= TOPPING_VOLTAGE && psu.getCurrent() < 300) {
-      psu.setConstantVoltage(FLOAT_VOLTAGE);
-    } else {
-      if(millis() - absorptionStart > 3000){
-        if(psu.getCurrent() < 100) {
-          Serial.println("Connect the battery");
-          while(1);
-        }
+  if(isControlTime()){
+    psu.servo();
+    avgCurrent = (float)psu.getCurrent() * 0.005 + (float)avgCurrent*0.995;
+    avgVoltage = (float)psu.getVoltage() * 0.005 + (float)avgVoltage*0.995;
+    
+    if(MODE_BULK == mode) {
+      if(avgVoltage > absorptionVoltage + voltsPerDegreeCompensation()) {
+        startAbsorptionStage();
+      }
+    } else if(MODE_ABSORPTION == mode) {
+      if(avgVoltage >= absorptionVoltage + voltsPerDegreeCompensation() && avgCurrent < bulkEndCurrent) {
+        startFloatStage();
+      }
+    } else if(MODE_FLOAT == mode) {
+    } else if(MODE_PRECHARGE == mode) {
+      if(stageStarted > 3000 && psu.getVoltage() > criticallyDischargedVoltage + voltsPerDegreeCompensation()) {
+        startBulkStage();
       }
     }
     
-    
- } else if(MODE_CC == psu.getMode() && isVoltageControlTime()) {
-    Serial.print("CC, ");
-    
-    if(psu.getVoltage() > TOPPING_VOLTAGE + 100) {
-      psu.setConstantVoltage(TOPPING_VOLTAGE);
-      absorptionStart = millis();
-    }
-  }  
-  Serial.print(psu.getVoltage());
-  Serial.print(", ");
-  Serial.print(psu.getCurrent());
-  Serial.print(", ");
-  Serial.print(avgCurrent);
-  Serial.println();
+    checkBatteryConnected();
 
+    Serial.print(psu.getVoltage());
+    Serial.print(", ");
+    Serial.print(psu.getCurrent());
+    Serial.print(", ");
+    Serial.print(avgCurrent);
+    Serial.println();
+  }
 }
 
--- src/smpsCharger.h
+++ src/smpsCharger.h
@@ -3,7 +3,7 @@
 
 #define MODE_PRECHARGE 0
 #define MODE_BULK 1
-#define MODE_TOPPING 2
+#define MODE_ABSORPTION 2
 #define MODE_FLOAT 3
 
 #endif
